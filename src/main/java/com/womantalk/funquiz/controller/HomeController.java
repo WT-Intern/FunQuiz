b4df607d2653e15b8ee3bf61f34a6cb2c193c7ab
dewikmarpaung <dewikmarpaung@gmail.com>
2017-07-04 11:12:04 +0700

    update 4 July 2017

--- src/main/java/com/womantalk/funquiz/controller/HomeController.java
+++ src/main/java/com/womantalk/funquiz/controller/HomeController.java
@@ -5,8 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("home")
+    @RequestMapping("index")
     public String home() {
         return "index";
+    }
+
+    @RequestMapping("data")
+    public String homea() {
+        return "data";
+    }
+
+    @RequestMapping("addQuiz")
+    public String homeb() {
+        return "addQuiz";
+    }
+
+    @RequestMapping("edit")
+    public String homec() {
+        return "edit";
     }
 }
