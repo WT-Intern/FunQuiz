5e1f4135acc50a8af9c3662715677f18adb78cf2
glorialimbong <oli_limbong@yahoo.com>
2017-07-28 17:38:23 +0700

    finish function

---
+++ src/main/java/com/womantalk/funquiz/tools/View.java
@@ -0,0 +1,13 @@
+package com.womantalk.client.tools;
+
+/**
+ * Created by gloria on 14/07/2017.
+ */
+public class View {
+
+   // public interface Base{}
+
+    public interface Base{};
+    public interface QuizRules extends Base{};
+    public interface Public extends Base {};
+}
